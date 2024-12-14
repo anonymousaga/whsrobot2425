6eddf47a4036912fc4ddda21dcecc76bf6d0dc66
Anonymous <68388009+anonymousaga@users.noreply.github.com>
2024-12-13 21:24:44 -0500

    prepare for competition

--- rv.py
+++ rv.py
@@ -3,67 +3,17 @@
 
 silent = False
 
-targetTime = 72.69
+targetTime = 60
 
 # ACCEL VARS
 # SPEED VARS in cm/s
-saccel = 4.35 #3.7 #5.45
+saccel = 4.1 #3.7 #5.45
 turnSpeedDefault = 95 #90 #120
 maxstraightSpeed = 145 #100 #145
 
 
 commands="""
 s32
-r
-s100
-l
-s125
-ld
-sd35
-u
-sd35
-rd
-s50
-rd
-sd25
-r
-sd50
-ld
-s75
-r
-s50
-r
-s40
-u
-s90
-l
-s40
-u
-s40
-r
-s25
-rd
-sd50
-ld
-s75
-r
-s75
-rd
-sd25
-r
-sd30
-u
-sd30
-l
-sd25
-ld
-s75
-l
-s100
-r
-s50
-l
-s44
 """
 
 commands4 = """
