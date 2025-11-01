b7a8f8e35dcf43ef313c92537554db71d92392a4
Anonymous <68388009+anonymousaga@users.noreply.github.com>
2025-11-01 14:57:02 -0400

    new turn constants

--- rv.py
+++ rv.py
@@ -1,7 +1,7 @@
 # DOWEL OFFSET 7CM
 # IF INTIAL SPEED IS OVER 60, TURN UP THE ACCEL TO MAX
 
-SILENT = False
+SILENT = True
 
 TARGETTIME = 60
 
@@ -9,8 +9,8 @@ TARGETTIME = 60
 MAXSTRAIGHTSPEED = 200 #100 #200
 
 # TURN VARS
-lturnsteps = 13.744
-rturnsteps = 13.844
+lturnsteps = 13.829
+rturnsteps = 13.829
 TCS_IMPORT = False
 
 commands="""
